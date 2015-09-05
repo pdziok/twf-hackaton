7a719081ef0248f66d6095a30a5d4eb889e858a4
Pawel Dziok <pawel.dziok@schibsted.pl>
2015-09-05 17:29:27 +0200

    Added dynamically recalculated elements in row number and dimensions

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -3,9 +3,12 @@ $(document).ready(function () {
         trigger: 'manual'
     });
 
-    var card = $(".game-card .card");
-    card.css('width', parseInt(new String(card.css('width')).replace(/[^0-9]/, '')) - 20);
-    card.css('height', parseInt(new String(card.css('height')).replace(/[^0-9]/, '')) - 20);
+    function relalculateCardDims() {
+        var card = $(".game-card .card");
+        var parent = card.parent();
+        card.css('width', parseInt(new String(parent.css('width')).replace(/[^0-9]/, '')) - 20);
+        card.css('height', parseInt(new String(parent.css('height')).replace(/[^0-9]/, '')) - 20);
+    }
 
     $('.game-card-container').click(function () {
         var $card = $(this).find('.game-card');
@@ -61,11 +64,15 @@ $(document).ready(function () {
         var cardAmount = $cards.find('.game-card').length;
         var ratio = $('body').width() / $('body').height();
         //var cardsPerRow = Math.sqrt(cardAmount) * ratio;
-        var desiredNumberPerRow = Math.ceil(cardAmount / (3 * ratio));
+        var desiredNumberPerRow = Math.ceil(cardAmount / Math.max(4 * ratio, 3));
         var cardsPerRow = cardAmount / desiredNumberPerRow;
         $cards.removeClass('small-up-1 small-up-2 small-up-3 small-up-4 small-up-5 small-up-6 small-up-7 small-up-8 small-up-9  small-up-10')
         $cards.addClass('small-up-' + Math.round(cardsPerRow))
     }
     recalculateElementWidth();
-    $(window).resize(recalculateElementWidth);
+    relalculateCardDims();
+    $(window).resize(function() {
+        recalculateElementWidth();
+        relalculateCardDims();
+    });
 });
