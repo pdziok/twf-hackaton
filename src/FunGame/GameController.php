3aad7fdb9bb9a9cfec970bd8727489ee66f4869f
Krzysztof Rudowski <rudi23@o2.pl>
2015-09-05 16:40:31 +0200

    play

--- src/FunGame/GameController.php
+++ src/FunGame/GameController.php
@@ -10,18 +10,18 @@ use Symfony\Component\HttpFoundation\Request;
 class GameController
 {
     private $imageList = [
-        ['id' => 1, 'href' =>'cards/1.jpg'],
-        ['id' => 2, 'href' =>'cards/2.jpg'],
-        ['id' => 3, 'href' =>'cards/3.jpg'],
-        ['id' => 4, 'href' =>'cards/4.jpg'],
-        ['id' => 5, 'href' =>'cards/5.jpg'],
-        ['id' => 6, 'href' =>'cards/6.jpg'],
-        ['id' => 7, 'href' =>'cards/7.jpg'],
-        ['id' => 8, 'href' =>'cards/8.jpg'],
-        ['id' => 9, 'href' =>'cards/9.jpg'],
-        ['id' => 10, 'href' =>'cards/10.jpg'],
-        ['id' => 11, 'href' =>'cards/11.jpg'],
-        ['id' => 12, 'href' =>'cards/12.jpg'],
+        ['id' => 1, 'href' => 'cards/1.jpg'],
+        ['id' => 2, 'href' => 'cards/2.jpg'],
+        ['id' => 3, 'href' => 'cards/3.jpg'],
+        ['id' => 4, 'href' => 'cards/4.jpg'],
+        ['id' => 5, 'href' => 'cards/5.jpg'],
+        ['id' => 6, 'href' => 'cards/6.jpg'],
+        ['id' => 7, 'href' => 'cards/7.jpg'],
+        ['id' => 8, 'href' => 'cards/8.jpg'],
+        ['id' => 9, 'href' => 'cards/9.jpg'],
+        ['id' => 10, 'href' => 'cards/10.jpg'],
+        ['id' => 11, 'href' => 'cards/11.jpg'],
+        ['id' => 12, 'href' => 'cards/12.jpg'],
     ];
 
     public function getStartAction(Request $request, SilexApplication $app)
@@ -34,19 +34,49 @@ class GameController
         ]);
     }
 
-    public function getMainAction(Request $request, SilexApplication $app)
+    public function getPlayAction(Request $request, SilexApplication $app)
     {
-        $amount = $request->get('amount', 3);
-        $cards = $this->getRandomCards($amount);
+        $amount = $request->get('amount', 20);
+        $chosenIds = $request->get('chosen');
 
-        return $app['twig']->render('game/main.twig', [
-            'cards' => $cards
+        $allCardsNumber = 20;
+        $cardTypesNumber = 6;
+        $chosenIds = [1, 2, 3];
+
+        $cardIds = $this->getCardIdsToPlay($allCardsNumber, $cardTypesNumber, $chosenIds);
+        $cards = $this->getCards($cardIds, $chosenIds);
+
+        return $app['twig']->render('game/play.twig', [
+            'cards' => $cards,
         ]);
     }
 
     public function getSummaryAction(SilexApplication $app)
     {
         return $app['twig']->render('game/summary.twig');
+    }
+
+    private function getCards(array $cardIds, array $chosenIds)
+    {
+        $cards = [];
+        foreach ($this->imageList as $image) {
+            foreach ($cardIds as $cardId) {
+                if ($image['id'] == $cardId) {
+
+                    if(in_array($image['id'], $chosenIds)){
+                        $image['chosen'] = 1;
+                    } else {
+                        $image['chosen'] = 0;
+                    }
+
+                    $cards[] = $image;
+                }
+            }
+        }
+
+        shuffle($cards);
+
+        return $cards;
     }
 
     /**
@@ -65,5 +95,53 @@ class GameController
         }
 
         return $cards;
+    }
+
+    /**
+     * @param integer $allCardsNumber  liczba kart na planszy
+     * @param integer $cardTypesNumber liczba typow kart
+     * @param array $chosenCards wybrane id kart
+     * @return array
+     */
+    private function getCardIdsToPlay($allCardsNumber, $cardTypesNumber, array $chosenCards)
+    {
+        $oneCardNumber = floor($allCardsNumber / $cardTypesNumber);
+
+        //cards to guess
+        $toGuessCardIds = [];
+        for ($i = 0; $i < $oneCardNumber; $i++) {
+            $toGuessCardIds = array_merge($chosenCards, $toGuessCardIds);
+        }
+
+        //cards other than to guess
+        $otherCardIds = array_diff($this->extractCardIds($this->imageList), $chosenCards);
+        $otherCardIds = array_slice($otherCardIds, 0, $cardTypesNumber - count($chosenCards));
+
+        //wrong cards
+        $wrongCardIds = [];
+        for ($i = 0; $i < $oneCardNumber; $i++) {
+            $wrongCardIds = array_merge($otherCardIds, $wrongCardIds);
+        }
+
+        $cardsToAdd = $allCardsNumber - $oneCardNumber * count($chosenCards) - $oneCardNumber * count($otherCardIds);
+        for ($i = 0; $i < $cardsToAdd; $i++) {
+            $wrongCardIds[] = array_rand($otherCardIds, 1);
+        }
+
+        $allCardIds = array_merge($toGuessCardIds, $wrongCardIds);
+
+        shuffle($allCardIds);
+
+        return $allCardIds;
+    }
+
+    private function extractCardIds($cards)
+    {
+        $ids = [];
+        foreach ($cards as $card) {
+            $ids[] = $card['id'];
+        }
+
+        return $ids;
     }
 }
