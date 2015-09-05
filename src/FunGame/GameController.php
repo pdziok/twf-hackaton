1ca300d27ec1f84c35b3ec6fcd3e16f2a44e7b40
Pawel Dziok <pawel.dziok@schibsted.pl>
2015-09-05 11:06:48 +0200

    Added game start screen

--- src/FunGame/GameController.php
+++ src/FunGame/GameController.php
@@ -10,6 +10,20 @@ class GameController
 {
     public function getStartAction(SilexApplication $app)
     {
-        return $app['twig']->render('game/start.twig');
+        return $app['twig']->render('game/start.twig', [
+            'cards' => [
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+                ['href' => 'http://ocdn.eu/images/pulscms/OTY7MDQsMCwxNywzMjAsMWMyOzA2LDMxNCwxYmM7MGMsMTQwYjFjZmU3ZjBhYzUyZWRjMDEwZDcwOTc4ZTg0YmUsMSwxLDYsMA__/1ab5be982c5d11f9091e92b759cd801e.jpg'],
+            ]
+        ]);
     }
 }
